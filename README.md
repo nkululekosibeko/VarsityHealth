5a7ad9af4b33be64aa760621d6a54b0be29756b9
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-10-02 06:35:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Varsity Health
+ This is a booking system for university students too book an appointnents in the doctor facilities in varcity.
---
+++ app/src/main/java/com/example/varsityhealth/SignupActivity.java
@@ -0,0 +1,37 @@
+package com.example.varsityhealth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignupActivity extends AppCompatActivity {
+
+    // Buttons
+    Button signUp_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_signup);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signup_screen), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        signUp_btn = findViewById(R.id.signup_button);
+
+        signUp_btn.setOnClickListener(v -> {
+            Intent intent = new Intent(SignupActivity.this,LoginActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
